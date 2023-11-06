cb2d24c1a4f7be4aba9e343d86afe4043b1e9631
Kyrylo Lubynets <kyrylo.lybunets@idealsalessolutions.co.uk>
2023-11-06 16:07:49 +0000

    init commit

---
+++ config/modules.config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+];
